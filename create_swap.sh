839f718db513eb4802c51f6b316cd45a8de262d8
pogin503 <pogin503@gmail.com>
2014-07-02 19:50:19 +0900

    Suppress warning and Display swapfile state

--- create_swap.sh
+++ create_swap.sh
@@ -3,17 +3,28 @@
 # Exit on any error (non-zero return code)
 set -e
 
+swapon -s
+
+swapoff -a
+
+[ -f /swapfile ] && rm -f /swapfile
+
 # Create swapfile of 2GB with block size 1MB
 #/bin/dd if=/dev/zero of=/swapfile bs=1024 count=2097152
 
 # Create swapfile of 1GB with block size 1MB
-/bin/dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+# /bin/dd if=/dev/zero of=/swapfile bs=1024 count=1048576
+fallocate -l 1G /swapfile
 
 # Set up the swap file
 /sbin/mkswap /swapfile
 
+chmod 600 /swapfile
+
 # Enable swap file immediately
 /sbin/swapon /swapfile
+
+swapon -s
 
 # Enable swap file on every boot
 /bin/echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
