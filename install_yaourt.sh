0d93ae97e4004572a92789d7f21eaab2b853a893
pogin503 <pogin503@gmail.com>
2014-07-29 16:56:44 +0900

    Add " && \ " to end of line

--- install_yaourt.sh
+++ install_yaourt.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
+
 which rbenv > /dev/null 2>&1
 if [ $? -eq 1 ]; then
-    curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
-    tar zxvf package-query.tar.gz
-    cd package-query
-    makepkg -si --noconfirm --needed
-    cd ..
-    curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
-    tar zxvf yaourt.tar.gz
-    cd yaourt
-    makepkg -si --noconfirm --needed
-    cd ..
-    rm package-query.tar.gz
-    rm -rf package-query
-    rm yaourt.tar.gz
+    curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz && \
+    tar zxvf package-query.tar.gz && \
+    cd package-query && \
+    makepkg -si --noconfirm --needed && \
+    cd .. && \
+    curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz && \
+    tar zxvf yaourt.tar.gz && \
+    cd yaourt && \
+    makepkg -si --noconfirm --needed && \
+    cd .. && \
+    rm package-query.tar.gz && \
+    rm -rf package-query && \
+    rm yaourt.tar.gz && \
     rm -rf yaourt
 fi
 
