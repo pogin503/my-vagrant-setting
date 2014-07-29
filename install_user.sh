a466a1153ac5ac68f82813e23fabdcefdee0d3c0
pogin503 <pogin503@gmail.com>
2014-07-29 16:55:28 +0900

    Update path setting, rbenv install ruby 2.1.2

--- install_user.sh
+++ install_user.sh
@@ -14,11 +14,16 @@ curl -L http://install.ohmyz.sh | sh
 # rbenv
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.profile
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.profile
-echo 'eval "$(rbenv init -)"' >> ~/.zshenv
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 
 git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+rbenv install 2.1.2
+rbenv global 2.1.2
+rbenv rehash
+rbenv versions
 
 cp ~/.zshrc ~/.zshrc.orig
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
