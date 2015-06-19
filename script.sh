a1c0180bc6e70ae7828029708e4084cfd04bcf83
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-20 00:29:21 +0900

    他のバッケージからインストールされるパッケージを削除

--- script.sh
+++ script.sh
@@ -10,13 +10,11 @@ pacman -S archlinux-keyring --noconfirm
 pacman -Su --noconfirm
 ]pacman-db-upgrade
 pacman -S base-devel --noconfirm --needed
-pacman -S ruby --noconfirm --needed
 pacman -S python --noconfirm --needed
 pacman -S clojure --noconfirm --needed
 pacman -S git zsh tmux --noconfirm --needed
 pacman -S emacs-nox vim --noconfirm --needed
 pacman -S ghc cabal-install happy --noconfirm --needed
-pacman -S sqlite --noconfirm --needed
 pacman -S virtualbox-guest-utils --noconfirm --needed
 
 sed -i -e "s/#ja_JP.UTF-8/ja_JP.UTF-8/" /etc/locale.gen
