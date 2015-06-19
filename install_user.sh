73e91d714c7671b09408a86bd23f293ab4c1007e
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-06-20 00:30:02 +0900

    Update rbenv setting

--- install_user.sh
+++ install_user.sh
@@ -20,16 +20,18 @@ sudo chsh -s $(which zsh) vagrant
 
 # rbenv
 git clone git://github.com/sstephenson/rbenv.git .rbenv
+git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 echo 'eval "$(rbenv init -)"' >> ~/.zshrc
 
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bash_profile
 
 # rbenv
-rbenv install 2.1.2
-rbenv global 2.1.2
+rbenv install 2.2.2
+rbenv global 2.2.2
 rbenv rehash
 rbenv versions
 
