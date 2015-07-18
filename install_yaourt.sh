160d83e7d35426924c9343f7bcdd937e476f975d
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2015-07-19 04:24:12 +0900

    Update the installation process of yaourt

--- install_yaourt.sh
+++ install_yaourt.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-which rbenv > /dev/null 2>&1
-if [ $? -eq 1 ]; then
-    curl -O https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz && \
-    tar zxvf package-query.tar.gz && \
-    cd package-query && \
-    makepkg -si --noconfirm --needed && \
-    cd .. && \
-    curl -O https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz && \
-    tar zxvf yaourt.tar.gz && \
-    cd yaourt && \
-    makepkg -si --noconfirm --needed && \
-    cd .. && \
-    rm package-query.tar.gz && \
-    rm -rf package-query && \
-    rm yaourt.tar.gz && \
-    rm -rf yaourt
-fi
+sudo sh -c 'echo "" >> /etc/pacman.conf'
+sudo sh -c 'echo "[archlinuxfr]" >> /etc/pacman.conf'
+sudo sh -c 'echo "SigLevel = Never" >> /etc/pacman.conf'
+sudo sh -c 'echo "Server = http://repo.archlinux.fr/\$arch" >> /etc/pacman.conf'
 
-yaourt -Syu --devel --aur
-# {
-# echo [archlinuxfr]
-# echo SigLevel = Never
-# echo Server = http://repo.archlinux.fr/$arch
-# } >> /etc/pacman.conf
+sudo pacman -Sy yaourt --noconfirm --needed
